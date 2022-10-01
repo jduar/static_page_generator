7af3a631508f06946704b0e5e70e3aab30bebf57
André Silva <33263706+Kamuish@users.noreply.github.com>
2022-10-01 15:25:18 +0100

    re-working the logic behind the sorters + allow to select different ordering (#20)
    * changing os with pathlib.Path
    
    * adding type hints to functions
    
    * reworking "dispatch" function
    
    * fixing color sort
    
    * allow to control the order of the returned photos

--- utils/sorters.py
+++ utils/sorters.py
@@ -10,23 +10,26 @@ class OrderMethod:
     COLOR = "color"
 
 
-def sort_photos(photos: List[Photo], order_method: str):
-    if order_method == OrderMethod.DATE:
-        return date_sort(photos)
-    elif order_method == OrderMethod.COLOR:
-        return color_sort(photos)
-    else:
-        print("Not sorted.")
+def sort_photos(photos: List[Photo], order_method: str, reverse_order=True):
+    sorting_map = {"date": date_sort,
+                   "color": color_sort,
+                   }
+    try:
+        return sorting_map[order_method](photos, reverse_order)
+
+    except KeyError:
+        print("Can't recognize sorting option, skipping it!")
 
 
-def date_sort(photos: List[Photo]) -> List[Photo]:
-    return sorted(photos, key=lambda x: x.original_date, reverse=True)
+def date_sort(photos: List[Photo], reverse_order: bool) -> List[Photo]:
+    return sorted(photos, key=lambda x: x.original_date, reverse=reverse_order)
 
 
-def color_sort(photos: List[Photo]) -> List[Photo]:
+def color_sort(photos: List[Photo], reverse_order: bool) -> List[Photo]:
     photo_list = []
     for photo in photos:
         H, _, _ = Image.open(photo.path).convert("RGB").convert("HSV").split()
-        photo_list.append((photo.path, ImageStat.Stat(H).mean))
-    sorted_list = sorted(photo_list, key=lambda x: x[1])
+        photo_list.append((photo, ImageStat.Stat(H).mean))
+    sorted_list = sorted(photo_list, key=lambda x: x[1], reverse=reverse_order)
     return [image[0] for image in sorted_list]
+
