b58d5438382c1286963860215431164b72204f5c
Table With Things On Top <jduar@protonmail.com>
2022-11-17 00:12:50 +0000

    Finished gallery rework.

--- template/js/gallery.js
+++ template/js/gallery.js
@@ -2,47 +2,54 @@ window.onload = buildGallery;
 window.onresize = buildGallery;
 
 var preferMaxHeight = 450;
+var padding = 5;
 
 function buildGallery() {
-  var gallery = document.getElementById("gallery");
-  var images = document.getElementsByClassName("image-outer");
+  var windowWidth = window.innerWidth;
+  if (windowWidth >= 865) {
+    // We only need CSS on small screens.
+    var gallery = document.getElementById("gallery");
+    var images = document.getElementsByClassName("image-outer");
 
-  var maxWidth = gallery.offsetWidth - 1;
+    var maxWidth = gallery.offsetWidth - 1;
 
-  let row = 0;
-  let rowWidth = 0;
+    let row = 0;
+    let rowWidth = 0;
 
-  let positionInRow = 0;
+    let positionInRow = 0;
 
-  for (let img_index = 0; img_index < images.length; img_index++) {
-    proposedWidth = calculateWidth(images[img_index]);
+    for (let img_index = 0; img_index < images.length; img_index++) {
+      proposedWidth = calculateWidth(images[img_index]);
 
-    if (rowWidth + proposedWidth <= maxWidth) {
-      rowWidth += proposedWidth;
-      positionInRow++;
-      console.log("menor", proposedWidth);
-    } else if (rowWidth + proposedWidth > maxWidth && positionInRow === 0) {
-      images[img_index].height = calculateHeight(images[img_index], maxWidth);
-      row++;
-    } else if (rowWidth + proposedWidth > maxWidth && positionInRow > 0) {
-      var imagesToAdjust = [];
-      for (let position = 0; position <= positionInRow; position++) {
-        imagesToAdjust.push(images[img_index - (positionInRow - position)]);
+      if (rowWidth + proposedWidth + 2 * padding <= maxWidth) {
+        rowWidth += proposedWidth;
+        positionInRow++;
+      } else if (
+        rowWidth + proposedWidth + 2 * padding > maxWidth &&
+        positionInRow === 0
+      ) {
+        images[img_index].height = calculateHeight(images[img_index], maxWidth);
+        row++;
+      } else if (
+        rowWidth + proposedWidth + 2 * padding > maxWidth &&
+        positionInRow > 0
+      ) {
+        var imagesToAdjust = [];
+        for (let position = 0; position <= positionInRow; position++) {
+          imagesToAdjust.push(images[img_index - (positionInRow - position)]);
+        }
+        adjustImagesToFit(imagesToAdjust, maxWidth);
+
+        row++;
+        rowWidth = 0;
+        positionInRow = 0;
       }
-      console.log("row", row);
-      adjustImagesToFit(imagesToAdjust, maxWidth);
-
-      row++;
-      rowWidth = 0;
-      positionInRow = 0;
-    } else {
-      console.log("coco");
     }
   }
-};
+}
 
 function getImageWithin(div) {
-  return div.getElementsByClassName("image-temp")[0];
+  return div.getElementsByClassName("gallery-image")[0];
 }
 
 function calculateWidth(div, height = preferMaxHeight) {
@@ -64,27 +71,27 @@ function adjustImagesToFit(divs, maxWidth) {
   var ratios = [];
   for (let i = 0; i < divs.length; i++) {
     var image = getImageWithin(divs[i]);
-    // ratios.push(preferMaxHeight / image.naturalHeight);
     ratios.push(image.naturalWidth / image.naturalHeight);
   }
   const realRatio =
     maxWidth / ratios.reduce((partialSum, a) => partialSum + a, 0);
-  console.log("maxWidth", maxWidth);
-  console.log(ratios.reduce((partialSum, a) => partialSum + a, 0));
-  console.log("realRatio", realRatio);
   for (let i = 0; i < divs.length; i++) {
-    console.log(
-      "ratio",
-      ratios[i],
-      "height",
-      calculateHeight(divs[i], realRatio * ratios[i]),
-      "width",
-      realRatio * ratios[i]
-    );
     setSize(
       divs[i],
       calculateHeight(divs[i], realRatio * ratios[i]),
-      realRatio * ratios[i]
+      realRatio * ratios[i] - 2 * padding
     );
+  }
+}
+
+function setPreferredMaxHeight() {
+  if (windowWidth >= 865 && windowWidth < 1080) {
+    preferMaxHeight = 180;
+  } else if (windowWidth >= 1080 && windowWidth < 1920) {
+    preferMaxHeight = 290;
+  } else if (windowWidth >= 1080 && windowWidth < 2560) {
+    preferMaxHeight = 350;
+  } else if (windowWidth >= 2560) {
+    preferMaxHeight = 450;
   }
 }
