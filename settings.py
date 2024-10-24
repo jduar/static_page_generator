124267afe4fc3c166d9c5616405dd09a958a8945
João Duarte <jduar@protonmail.com>
2024-10-24 01:19:00 +0100

    Rework generation code and class structure.

--- settings.py
+++ settings.py
@@ -1,5 +1,11 @@
-USE_CATEGORIES = False
-CATEGORIES = []
-# Used as tab title and main page title
 TITLE="example.xyz"
 DESCRIPTION="An example image gallery"
+
+# If the whitelist is not empty, only tags defined in it be displayed as website sections.
+# Tags defined in the blacklist won't be displayed as website sections. The whitelist takes precedence
+# over the blacklist. If both are empty, all tags will be displayed as website sections.
+# Example usage: TAGS_WHITELIST = ["cat", "dog"]
+TAGS_WHITELIST = []
+TAGS_BLACKLIST = []
+# By default, tags are ordered alphabetically. Set TAG_CUSTOM_ORDER to True to follow the order in TAGS_WHITELIST.
+TAG_CUSTOM_ORDER = False
